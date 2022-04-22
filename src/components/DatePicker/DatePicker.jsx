d36be38b515733aa3db439b7e16ae662b46c2ca0
Brian Sánchez <brian.sanchez@inmediatum.com>
2022-04-22 10:15:45 -0500

    create DatePicker component

---
+++ src/components/DatePicker/DatePicker.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DatePicker = () => {
+  return (
+    <>
+        <label for="date-input">Pick a date</label>
+        <input type="date" id='date-input'/>
+    </>
+  )
+}
+
+export default DatePicker
