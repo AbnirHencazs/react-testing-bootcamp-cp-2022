c251dc152baba47d6aefa53cd17f92773d386e8a
Brian Sánchez <brian.sanchez@inmediatum.com>
2022-04-22 12:16:12 -0500

    Create Footer component and pass its tests

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <small>Project created during Wizeline Academy React Testing Bootcamp</small>
+    </footer>
+  )
+}
+
+export default Footer
