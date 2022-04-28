ff1a2ed1d626a16e5dae13274f16b8c19af58503
Brian Sánchez <brian.sanchez@inmediatum.com>
2022-04-28 17:25:50 -0500

    make Home page pass tests

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
 import DatePicker from '../components/DatePicker'
 import POD from '../components/POD'
-import { mockPOD } from '../mocks/APOD/mockPOD'
+import usePOD from '../hooks/usePOD'
+import { DateTime } from 'luxon'
+import ReactLoading from 'react-loading'
+import './Home.module.css'
 
 const Home = () => {
+  const [ date, setDate ] = React.useState(DateTime.now())
+  const { pod, loading } = usePOD({ date: date.toFormat('yyyy-MM-dd') })
+
+  const changeDate = (e) => {
+    setDate(DateTime.fromFormat(e.target.value, 'yyyy-MM-dd'))
+  }
+
   return (
     <section>
-        <DatePicker/>
-        <POD item={mockPOD}/>
+        <DatePicker
+            date={date.toFormat('yyyy-MM-dd')}
+            handleChange={changeDate}/>
+        {
+            loading ?
+            <div style={{ display: 'flex', justifyContent: 'center'}}>
+                <ReactLoading data-testid="loading" type="spin" color="#9BDAF1" height={300} width={200} />
+            </div>
+            :
+            <POD item={pod}/>
+        }
     </section>
   )
 }
