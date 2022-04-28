f818a5760e69106830e4ab2422cdcdd847d968ad
Brian Sánchez <brian.sanchez@inmediatum.com>
2022-04-28 17:07:21 -0500

    implement styles on DatePicker

--- src/components/DatePicker/DatePicker.jsx
+++ src/components/DatePicker/DatePicker.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import './DatePicker.css'
 
-const DatePicker = () => {
+const DatePicker = ({ handleChange, date }) => {
   return (
-    <>
-        <label for="date-input">Pick a date</label>
-        <input type="date" id='date-input'/>
-    </>
+    <div className='datepickerContainer'>
+        <label htmlFor="date-input">Pick a date</label>
+        <input type="date" id='date-input' onChange={ handleChange } value={date} />
+    </div>
   )
 }
 
