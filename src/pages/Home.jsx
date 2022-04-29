ec4ad4b88b9f117241b81394112c79bf82334e55
Brian Sánchez <brian.sanchez@inmediatum.com>
2022-04-28 21:08:58 -0500

    disappear the alert emssage after 1.5 seconds

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,17 +5,29 @@ import usePOD from '../hooks/usePOD'
 import { DateTime } from 'luxon'
 import ReactLoading from 'react-loading'
 import './Home.module.css'
+import Alert from '../components/Alert'
 
 const Home = () => {
   const [ date, setDate ] = React.useState(DateTime.now())
-  const { pod, loading } = usePOD({ date: date.toFormat('yyyy-MM-dd') })
+  const [ showAlert, setShowAlert ] = React.useState(false)
+  const { pod, loading, error } = usePOD({ date: date.toFormat('yyyy-MM-dd') })
 
   const changeDate = (e) => {
     setDate(DateTime.fromFormat(e.target.value, 'yyyy-MM-dd'))
   }
 
+  React.useEffect(() => {
+    if(error){
+      setShowAlert(true)
+      setTimeout(() => {
+        setShowAlert(false)
+      }, 1500)
+    }
+  }, [error])
+
   return (
     <section>
+        <Alert show={showAlert} type='error' content={error?.data?.msg}/>
         <DatePicker
             date={date.toFormat('yyyy-MM-dd')}
             handleChange={changeDate}/>
